71b143d5ac5b61c83c5c2896e276445d4e7c1ce9
pranay1303 <150463014+pranay1303@users.noreply.github.com>
2025-02-26 00:57:20 +0530

    working  nav

--- src/App.jsx
+++ src/App.jsx
@@ -9,42 +9,9 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <nav className="nav">
-      <div className="logo">AyurHelp</div>
-
-      {/* Hamburger Icon */}
-      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "✖" : "☰"}
-      </div>
-
-      {/* Navigation Links */}
-      <div className={`nav-links ${isOpen ? "open" : ""}`}>
-        <Link to='' className="nav-btn" onClick={() => setIsOpen(false)}>
-          Home
-        </Link>
-
-        <Link to='contact' className="nav-btn" onClick={() => setIsOpen(false)}>
-          Contact
-        </Link>
-
-        <Link to='about' className="nav-btn" onClick={() => setIsOpen(false)}>
-          About
-        </Link>
-
-        <Link to='faq' className="nav-btn" onClick={() => setIsOpen(false)}>
-          FAQ
-        </Link>
-
-        <a href="#"  className="nav-btn" onClick={() => setIsOpen(!isOpen)}>
-          AyurBot
-        </a>
-
-
-      
-      </div>
-    </nav>
-      <Outlet/>
-      <Chatbot open={isOpen} />
+      <Navbar />
+      <Outlet />
+      <Chatbot />
     </>
   );
 }
